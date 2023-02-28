c5b8fed8a1acdeb48e69db265aaa5a84183dd139
Mauro <126135677+MauroGonzaleez@users.noreply.github.com>
2023-02-28 03:17:47 -0300

    Add files via upload

---
+++ miproyectodeprueba/punto1a.java
@@ -0,0 +1,16 @@
+package miproyectodeprueba;
+
+public class tarea1 {
+
+	public static void main(String[] args) {
+		int numeroInicio = 5;
+		int numeroFin = 14;
+
+		while(numeroInicio <= numeroFin) {
+		  System.out.print(numeroInicio+",");
+		  numeroInicio=numeroInicio+1;
+		}
+	}
+	}
+	
+		
---
+++ miproyectodeprueba/punto1c.java
@@ -0,0 +1,16 @@
+package miproyectodeprueba;
+
+public class tareac {
+	 public static void main(String[] args) {
+		 int a = 5;
+		 int b = 14;
+
+		 while (a <= b) {
+			 if(a % 2 != 0)
+		     System.out.print(a + ",");
+		     a++;
+		 }
+		 
+	 }
+}
+	
