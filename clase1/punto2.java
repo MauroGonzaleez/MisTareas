b4dae1394e16f14076599a08ebfb33d80eca7cd0
Mauro <126135677+MauroGonzaleez@users.noreply.github.com>
2023-02-28 03:23:22 -0300

    Add files via upload

---
+++ clase1/punto2.java
@@ -0,0 +1,27 @@
+package miproyectodeprueba;
+
+public class punto2 {
+	 public static void main(String[] args) {
+		 
+
+			float ingresos = 100000;
+			int vehiculos = 1;
+			int inmuebles = 1;
+			boolean embarcacionesoaeronave = false;
+			
+
+			if ((ingresos > 489083) || //
+					(vehiculos >= 3) || //
+					(inmuebles >= 3) || //
+					(embarcacionesoaeronave == true) //
+					
+					
+			) {
+				System.out.println("SI es de segmento alto");
+			} else {
+				System.out.println("NO es de segmento alto");
+			}
+		}
+	}
+	 
+		 
