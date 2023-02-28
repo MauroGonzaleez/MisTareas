56e4c8cb41a38b2768df1c716f9bce871864b936
Mauro <126135677+MauroGonzaleez@users.noreply.github.com>
2023-02-28 03:10:32 -0300

    Add files via upload

---
+++ miproyectodeprueba/src/miproyectodeprueba/D.java
@@ -0,0 +1,12 @@
+package miproyectodeprueba;
+
+public class D {
+	 public static void main(String[] args) {
+	int a = 5;
+	int b = 14;
+
+	for (int num = b; num >= a; num--) {
+	  System.out.print(num + ","); 
+	}
+}
+}
